a475ebe0e3ebd065045e4494057a1d806ca1f495
DimaM315 <40835161+DimaM315@users.noreply.github.com>
2023-03-16 20:10:30 +0500

    Update User.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -36,8 +36,15 @@ public class User {
       this.email = email;
       this.car = car;
    }
-   public Car getCar() {return car;}
-   public void setCar(Car car) {this.car = car;}
+   
+   public Car getCar() {
+      return car;
+   }
+   
+   public void setCar(Car car) {
+      this.car = car;
+   }
+   
    public Long getId() {
       return id;
    }
